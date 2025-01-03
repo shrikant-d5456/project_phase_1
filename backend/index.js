0e93cd865d99c27fc24c840f30566e885a8b09e1
Shrikant Vithoba Dalvi <157231725+shrikant-d5456@users.noreply.github.com>
2025-01-03 08:41:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173"],
     methods:["POST","GET","DELETE","PUT"],
 }));
 
