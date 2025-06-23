dc3b95564febab0d348347a5964d8d7ccc6e2225
shrikant-d5456 <dalvishrikant5456@gmail.com>
2025-06-23 22:57:50 +0530

    changes added

---
+++ frontend/src/components/Notify.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Bs2CircleFill } from 'react-icons/bs'
+
+const Notify = () => {
+  return (
+    <div>
+      <div className=' flex justify-between w-20 bg-white p-2'>
+        <div><Bs2CircleFill/></div>
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, suscipit.</div>
+      </div>
+    </div>
+  )
+}
+
+export default Notify
